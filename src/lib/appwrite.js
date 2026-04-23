023556c6402fb439ac39ee92d1f4f1c89b49d196
annamoyosore <annamoyosore@gmail.com>
2026-04-23 22:52:00 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -19,8 +19,8 @@ export { ID };
 export const DB_ID = "6970a6be003068d274d1";
 
 // 🔴 REPLACE THESE WITH YOUR REAL COLLECTION IDs
-export const USERS_COLLECTION_ID = "YOUR_USERS_COLLECTION_ID";
-export const TX_COLLECTION_ID = "YOUR_TX_COLLECTION_ID";
+export const USERS_COLLECTION_ID = "users";
+export const TX_COLLECTION_ID = "transactions";
 
 // (future)
 // export const GAMES_COLLECTION_ID = "...";
