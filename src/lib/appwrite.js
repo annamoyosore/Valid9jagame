2890d4db377940dac8190a0a636185c58b5f8818
annamoyosore <annamoyosore@gmail.com>
2026-04-23 22:21:24 +0100

    Update appwrite.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1") // default cloud
-  .setProject("YOUR_PROJECT_ID"); // 🔴 replace this
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1") // default cloud
+  .setProject("69cb4e5c001651f6cfab"); // 🔴 replace this
 
 export const account = new Account(client);
 export default client;
