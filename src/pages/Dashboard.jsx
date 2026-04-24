ddd89bdda5de769be994e52724f606327d118d55
annamoyosore <annamoyosore@gmail.com>
2026-04-24 17:43:19 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-
+import { useAuth } from "../context/AuthContext.jsx";
 export default function Dashboard() {
   const { user } = useAuth();
 
