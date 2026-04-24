fa741ac1e6479ad2ce349c120253378f405ad239
annamoyosore <annamoyosore@gmail.com>
2026-04-24 18:18:44 +0100

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext.jsx";
+import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
   if (loading) return <p>Loading...</p>;
 
+  // ❌ DON'T use /login anymore
   if (!user) return <Navigate to="/" />;
 
   return children;
